58947070f1bf2fe10c721dc60304c592f8eaed52
blokhinvalentin <blokhin@fortech.dev>
2022-07-04 16:04:06 +0300

    changing front #2

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let input = null;
 let allTasks = JSON.parse(localStorage.getItem('allTasks')) || [];
 let valueInput = '';
 const host = 'http://localhost:8000';
@@ -8,24 +7,28 @@ const hdrs = {
 };
 
 window.onload = async () => {
-  if (document.getElementById('addTask') !== null) {
-    input = document.getElementById('addTask');
-  } else {
-    console.error('no such element');
+  const input = document.getElementById('addTask');
+  if (input === null) {
+    return;
   }
-  
+
   input.focus();
   input.addEventListener('change', updateValue);
+
   const resp = await fetch(`${host}/allTasks`, {
     method: 'GET'
   });
   const result = await resp.json();
-  allTasks = result.data;
+  allTasks = result;
   localStorage.setItem('Tasks', JSON.stringify(allTasks));
   render();
 }
 
-const onClickAddTaskButton = async () => {
+const onClickAddTask = async () => {
+  const input = document.getElementById('addTask');
+  if (input === null) {
+    return;
+  }
   if (valueInput.trim() === '') {
     return;
   }
@@ -50,10 +53,10 @@ const updateValue = (event) => {
   valueInput = event.target.value;
 }
 
-const render = async () => {
+const render = () => {
   const content = document.getElementById('contentPage');
   if (content === null) {
-    console.error('no such element');
+    return;
   }
 
   while (content.firstChild) {
@@ -61,17 +64,19 @@ const render = async () => {
   }
 
   allTasks.forEach((item) => {
-    [itemId] = [item._id];
+    let { itemId, itemIsCheck } = { itemId: item._id, itemIsCheck: item.isCheck };
     let previousValue = '';
 
     const container = document.createElement('div');
     const checkbox = document.createElement('input');
     const text = document.createElement('p');
     const editButton = document.createElement('button');
-    const doneButton = document.createElement('button');
     const deleteButton = document.createElement('button');
-    const cancelButton = document.createElement('button');
-    const newText = document.createElement('input');
+    const editImg = document.createElement('img');
+    const deleteImg = document.createElement('img');
+
+    editImg.src = 'img/edit.svg';
+    deleteImg.src = 'img/close.svg';
 
     container.id = `${itemId}`;
     container.className = item.isCheck ? 'taskContainer Checked' : 'taskContainer Unchecked';
@@ -81,21 +86,11 @@ const render = async () => {
         
     [text.innerText, text.className] = [item.text, item.isCheck ? 'textTask doneText' : 'textTask'];
 
-
     editButton.className = item.isCheck ? 'textTask editButton' : 'buttonEdit';
-    editButton.style.backgroundImage = "url('img/edit.svg')";
-
-    doneButton.className = 'doneButton';
-    doneButton.style.backgroundImage = "url('img/done.svg')";
-
+    editButton.appendChild(editImg);
     
     deleteButton.className = 'deleteButton';
-    deleteButton.style.backgroundImage = "url('img/close.svg')";
-
-    cancelButton.className = 'cancelButton';
-    cancelButton.style.backgroundImage = "url('img/close.svg')";
-    
-    newText.type = 'text';
+    deleteButton.appendChild(deleteImg);
 
     container.appendChild(checkbox);
     container.appendChild(text);
@@ -103,27 +98,15 @@ const render = async () => {
     container.appendChild(deleteButton);
 
     checkbox.onchange = () => {
-      onChangeTaskCheckbox(container.id, checkbox.checked);
+      onChangeTaskCheckbox(itemId, itemIsCheck);
     };
 
     editButton.onclick = () => {
-      previousValue = onCLickEditTask(container.id, previousValue);
-      text.replaceWith(newText);
-      editButton.replaceWith(doneButton);
-      deleteButton.replaceWith(cancelButton);
-      newText.focus();
-    }
-
-    doneButton.onclick = () => {
-      onClickDoneTask(container.id, newText.value);
+      onCLickEditTask(itemId, previousValue, text, editButton, deleteButton);
     }
 
     deleteButton.onclick = () => {
-      onClickDeleteTask(container.id);
-    }
-
-    cancelButton.onclick = () => {
-      onClickCancelTaskEditing();
+      onClickDeleteTask(itemId);
     }
 
     content.appendChild(container);
@@ -131,12 +114,12 @@ const render = async () => {
 }
 
 const onChangeTaskCheckbox = async (id, check) => {
-  const resp = await fetch(`${host}/updateTask`, {
+  const resp = await fetch(`${host}/updateCheckbox`, {
     method: 'PATCH',
     headers: hdrs,
     body: JSON.stringify({
       _id: id,
-      isCheck: check
+      isCheck: !check
     })
   });
   const result = await resp.json();
@@ -145,25 +128,53 @@ const onChangeTaskCheckbox = async (id, check) => {
   render();
 }
 
-const onCLickEditTask = async (id, previousValue) => {
+const onCLickEditTask = async (id, previousValue, text, editButton, deleteButton) => {
+  const parent = document.getElementById(id);
+  if (parent === null) {
+    return;
+  }
+
+  const replacableText = document.createElement('input');
+  const doneButton = document.createElement('button');
+  const cancelButton = document.createElement('button');
+  const doneImg = document.createElement('img');
+  const cancelImg = document.createElement('img');
+  
+  input.type = 'text';
+
+  cancelButton.className = 'cancelButton';
+  doneButton.className = 'doneButton';
+  
+  doneImg.src = 'img/done.svg';
+  cancelImg.src = 'img/close.svg';
+  
+  doneButton.appendChild(doneImg);
+  cancelButton.appendChild(cancelImg);
+
+  replaceElems(id, text, editButton, doneButton, deleteButton, cancelButton, replacableText);
+
   previousValue = previousValue.innerText;
 
   return previousValue;
 }
 
 const onClickDoneTask = async (id, newValue) => {
-  const resp = await fetch(`${host}/updateTask`, {
-        method: 'PATCH',
-        headers: hdrs,
-        body: JSON.stringify({
-          _id: id,
-          text: newValue
-        })
-      });
-      const result = await resp.json();
-      allTasks = result;
-      localStorage.setItem('Tasks', JSON.stringify(allTasks));
-      render();
+  if (newValue.trim() === '') {
+    return onClickCancelTaskEditing();
+  }
+
+  const resp = await fetch(`${host}/updateText`, {
+    method: 'PATCH',
+    headers: hdrs,
+    body: JSON.stringify({
+      _id: id,
+      text: newValue
+    })
+  });
+  const result = await resp.json();
+  allTasks = result;
+  localStorage.setItem('Tasks', JSON.stringify(allTasks));
+  render();
 }
 
 const onClickDeleteTask = async (id) => {
@@ -173,22 +184,37 @@ const onClickDeleteTask = async (id) => {
     body: JSON.stringify({_id: id})
   });
   const result = await resp.json();
-  allTasks = result;
+  allTasks = allTasks.filter(task => task._id !== id);
   localStorage.setItem('Tasks', JSON.stringify(allTasks));
   render();
 }
 
 const onClickCancelTaskEditing = async (id, previousValue) => {
-  const resp = await fetch(`${host}/updateTask`, {
-    method: 'PATCH',
-    headers: hdrs,
-    body: JSON.stringify({
-      _id: id,
-      text: previousValue
-    })
-  });
-  const result = await resp.json();
-  allTasks = result;
-  localStorage.setItem('Tasks', JSON.stringify(allTasks));
+  // const resp = await fetch(`${host}/updateTask`, {
+  //   method: 'PATCH',
+  //   headers: hdrs,
+  //   body: JSON.stringify({
+  //     _id: id,
+  //     text: previousValue
+  //   })
+  // });
+  // const result = await resp.json();
+  // allTasks = result;
+  // localStorage.setItem('Tasks', JSON.stringify(allTasks));
   render();
+}
+
+const replaceElems = async (id, text, editButton, doneButton, deleteButton, cancelButton, replacableText) => {
+  text.replaceWith(replacableText);
+  editButton.replaceWith(doneButton);
+  deleteButton.replaceWith(cancelButton);
+  replacableText.focus();
+
+  doneButton.onclick = () => {
+    onClickDoneTask(id, replacableText.value);
+  }
+
+  cancelButton.onclick = () => {
+    onClickCancelTaskEditing();
+  }
 }
